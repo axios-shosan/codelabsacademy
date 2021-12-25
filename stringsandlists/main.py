5faa6099b8afcc59a86be8877e561e2479e17856
axios.shosan <medmohamed923@gmail.com>
2021-12-25 01:09:36 +0100

    string and list homework done

---
+++ stringsandlists/main.py
@@ -0,0 +1,12 @@
+even = list(range(1,300,2))
+
+def second_question():
+    print(len(even))
+    for number in even:
+        print(number ** 2)
+
+def third_question():
+    print(57 in even)
+
+second_question()
+third_question()
